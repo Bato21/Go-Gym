b307565e62fefd73daf91a4cbb628a2ad1e5e0e7
Ian Spikin <ian.spikin.t@gmail.com>
2026-09-22 17:01:23 -0300

    Actualiza perfil y logros

--- GG/src/app/logros/logros.page.ts
+++ GG/src/app/logros/logros.page.ts
@@ -1,19 +1,104 @@
 import { Component, OnInit } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { FormsModule } from '@angular/forms';
-import { IonContent, IonHeader, IonTitle, IonToolbar } from '@ionic/angular';
+
+import {
+  IonContent,
+  IonHeader,
+  IonTitle,
+  IonToolbar,
+  IonIcon,
+  IonProgressBar
+} from '@ionic/angular';
+
+import { addIcons } from 'ionicons';
+import {
+  trophyOutline,
+  barbellOutline,
+  flameOutline,
+  fitnessOutline,
+  medalOutline,
+  starOutline,
+  lockClosedOutline
+} from 'ionicons/icons';
 
 @Component({
   selector: 'app-logros',
   templateUrl: './logros.page.html',
   styleUrls: ['./logros.page.scss'],
-  imports: [IonContent, IonHeader, IonTitle, IonToolbar, CommonModule, FormsModule]
+  imports: [
+    IonContent,
+    IonHeader,
+    IonTitle,
+    IonToolbar,
+    IonIcon,
+    IonProgressBar,
+    CommonModule,
+    FormsModule
+  ]
 })
 export class LogrosPage implements OnInit {
 
-  constructor() { }
+  logros = [
+    {
+      icono: 'barbell-outline',
+      titulo: 'Primer entrenamiento',
+      descripcion: 'Completa tu primer entrenamiento',
+      completado: true
+    },
+    {
+      icono: 'flame-outline',
+      titulo: 'En llamas',
+      descripcion: 'Entrena 3 días seguidos',
+      completado: true
+    },
+    {
+      icono: 'trophy-outline',
+      titulo: 'Semana perfecta',
+      descripcion: 'Completa todos tus entrenamientos de la semana',
+      completado: false
+    },
+    {
+      icono: 'fitness-outline',
+      titulo: 'Constancia',
+      descripcion: 'Completa 10 entrenamientos',
+      completado: false
+    },
+    {
+      icono: 'medal-outline',
+      titulo: 'Atleta',
+      descripcion: 'Completa 25 entrenamientos',
+      completado: false
+    },
+    {
+      icono: 'star-outline',
+      titulo: 'Leyenda',
+      descripcion: 'Completa 100 entrenamientos',
+      completado: false
+    }
+  ];
 
-  ngOnInit() {
+  constructor() {
+
+    addIcons({
+      trophyOutline,
+      barbellOutline,
+      flameOutline,
+      fitnessOutline,
+      medalOutline,
+      starOutline,
+      lockClosedOutline
+    });
+
   }
 
+  ngOnInit() {}
+
+  get logrosCompletados() {
+    return this.logros.filter(logro => logro.completado).length;
+  }
+
+  get progreso() {
+    return this.logrosCompletados / this.logros.length;
+  }
 }
--- GG/src/app/perfil/perfil.page.ts
+++ GG/src/app/perfil/perfil.page.ts
@@ -1,16 +1,33 @@
 import { Component, OnInit } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { FormsModule } from '@angular/forms';
+
 import {
   IonContent,
   IonHeader,
-  IonItem,
-  IonLabel,
-  IonList,
-  IonListHeader,
   IonTitle,
   IonToolbar,
+  IonIcon,
+  IonProgressBar,
+  IonButton,
+  IonInput
 } from '@ionic/angular';
+
+import { addIcons } from 'ionicons';
+
+import {
+  personCircleOutline,
+  flameOutline,
+  calendarOutline,
+  barbellOutline,
+  trophyOutline,
+  fitnessOutline,
+  createOutline,
+  timeOutline,
+  starOutline,
+  saveOutline,
+  closeOutline
+} from 'ionicons/icons';
 
 @Component({
   selector: 'app-perfil',
@@ -19,21 +36,139 @@ import {
   imports: [
     IonContent,
     IonHeader,
-    IonItem,
-    IonLabel,
-    IonList,
-    IonListHeader,
     IonTitle,
     IonToolbar,
+    IonIcon,
+    IonProgressBar,
+    IonButton,
+    IonInput,
     CommonModule,
-    FormsModule,
+    FormsModule
   ],
 })
+
 export class PerfilPage implements OnInit {
 
-  constructor() { }
+  nombre = 'Ian';
+  apellido = 'Spikin Thomas';
+
+  diasRacha = 5;
+
+  objetivo = 'Ganar masa muscular';
+  rutinaFavorita = 'Push Pull Legs';
+
+  entrenamientosSemana = 3;
+  metaSemanal = 4;
+
+  editando = false;
+
+  // Copia temporal por si el usuario cancela
+  perfilTemporal: any = {};
+
+  proximosEntrenamientos = [
+    {
+      dia: 'Lunes 23',
+      entrenamiento: 'Pecho y tríceps',
+      hora: '18:00'
+    },
+    {
+      dia: 'Miércoles 25',
+      entrenamiento: 'Espalda y bíceps',
+      hora: '18:30'
+    },
+    {
+      dia: 'Viernes 27',
+      entrenamiento: 'Piernas',
+      hora: '17:30'
+    }
+  ];
+
+  constructor() {
+
+    addIcons({
+      personCircleOutline,
+      flameOutline,
+      calendarOutline,
+      barbellOutline,
+      trophyOutline,
+      fitnessOutline,
+      createOutline,
+      timeOutline,
+      starOutline,
+      saveOutline,
+      closeOutline
+    });
+
+  }
 
   ngOnInit() {
+
+    this.cargarPerfil();
+
+  }
+
+  get progresoSemanal() {
+    return this.entrenamientosSemana / this.metaSemanal;
+  }
+
+
+  editarPerfil() {
+
+    this.perfilTemporal = {
+      nombre: this.nombre,
+      apellido: this.apellido,
+      objetivo: this.objetivo,
+      rutinaFavorita: this.rutinaFavorita
+    };
+
+    this.editando = true;
+
+  }
+
+
+  guardarPerfil() {
+
+    const perfil = {
+      nombre: this.nombre,
+      apellido: this.apellido,
+      objetivo: this.objetivo,
+      rutinaFavorita: this.rutinaFavorita
+    };
+
+    localStorage.setItem('perfilUsuario', JSON.stringify(perfil));
+
+    this.editando = false;
+
+  }
+
+
+  cancelarEdicion() {
+
+    this.nombre = this.perfilTemporal.nombre;
+    this.apellido = this.perfilTemporal.apellido;
+    this.objetivo = this.perfilTemporal.objetivo;
+    this.rutinaFavorita = this.perfilTemporal.rutinaFavorita;
+
+    this.editando = false;
+
+  }
+
+
+  cargarPerfil() {
+
+    const perfilGuardado = localStorage.getItem('perfilUsuario');
+
+    if (perfilGuardado) {
+
+      const perfil = JSON.parse(perfilGuardado);
+
+      this.nombre = perfil.nombre;
+      this.apellido = perfil.apellido;
+      this.objetivo = perfil.objetivo;
+      this.rutinaFavorita = perfil.rutinaFavorita;
+
+    }
+
   }
 
 }
